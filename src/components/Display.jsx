afcdfb1902a2e601d0025782667435f1270a6146
unknown <spookymhrzn@gmail.com>
2024-06-08 22:30:05 +0545

    first commit

---
+++ src/components/Display.jsx
@@ -0,0 +1,13 @@
+import styles from "./Display.module.css";
+
+function Display({ displayValue }) {
+  return (
+    <input
+      className={styles.display}
+      type="text"
+      value={displayValue}
+      readOnly
+    />
+  );
+}
+export default Display;
